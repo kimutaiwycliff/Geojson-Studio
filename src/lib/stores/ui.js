da24c491dbe254980dd45e264e95a2999fe71b62
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2026-03-05 09:53:46 +0300

    feat: set default dark mode and dark basemap

--- src/lib/stores/ui.js
+++ src/lib/stores/ui.js
@@ -1,8 +1,8 @@
 import { writable } from 'svelte/store';
 
 export const sidebarOpen = writable(true);
-export const darkMode = writable(false);
+export const darkMode = writable(true);
 export const activeLayerId = writable(null);
-export const activeBasemap = writable('streets');
+export const activeBasemap = writable('dark');
 export const mouseCoords = writable('');
 export const statusMessage = writable('');
